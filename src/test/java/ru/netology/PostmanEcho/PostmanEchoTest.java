52fbc714251556315bc5176085dee594431b84c8
Irina_Selezneva <alirina238@gmail.com>
2023-05-01 16:09:39 +0300

    test passed

--- src/test/java/ru/netology/PostmanEcho/PostmanEchoTest.java
+++ src/test/java/ru/netology/PostmanEcho/PostmanEchoTest.java
@@ -18,7 +18,7 @@ public class PostmanEchoTest {
 
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("files"))
+                .body("data", equalTo("some data"))
         ;
     }
 
